86993f2f8e048d0ff1c926a504d8db22f826f606
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-18 08:17:46 +0000

    t

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,12 +3,14 @@
 /**
  * main - Entry point
  *
+ * Description: Prints a specific string to the console
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+    printf("\"with proper grammar, but the outcome is a piece of art,\"\n");
 
-	return (0);
+    return (0);
 }
