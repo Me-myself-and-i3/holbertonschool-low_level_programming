b127e3e326667786e6960a985e65d37eefefdebf
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-19 13:57:19 +0000

    t

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * new - Prints numbers from 0 to 9 separated by a comma and space
  */
 void new(void)
 {
 	int i = 0;
 	char c;
-	
+
 	for (; i < 10; i++)
 	{
 		if (i > 0)
@@ -22,6 +21,11 @@ void new(void)
 	putchar('\n'); /* To add a newline after the loop */
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	new();
