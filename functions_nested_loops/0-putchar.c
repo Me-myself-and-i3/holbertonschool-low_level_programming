5172dbace51110aee219f09cb122ce1f11645590
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-21 07:51:52 +0000

    t

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,19 +3,11 @@
 /**
  * main - Entry point of the program
  *
+ * Description: Prints "_putchar" followed by a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-
-	return (0);
+    printf("_putchar\n");
+    return (0);
 }
