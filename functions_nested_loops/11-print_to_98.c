a27d0cfe8408c4f2247bb7b22a5c4fe08fe8c371
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-23 10:58:04 +0000

    last

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,12 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: starting number
  *
- * Description: Prints all numbers from n to 98, separated by commas and tabs,
- *              and ends with a new line.
+ * Description: Prints all numbers from n to 98, separated by commas and spaces,
+ *              ending with a newline.
  */
 void print_to_98(int n)
 {
@@ -17,7 +18,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",\t");
+				printf(", ");
 		}
 	}
 	else
@@ -26,7 +27,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",\t");
+				printf(", ");
 		}
 	}
 	printf("\n");
