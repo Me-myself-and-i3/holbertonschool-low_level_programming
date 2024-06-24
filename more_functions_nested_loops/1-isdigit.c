a24a6230d974d13bad9568d345bcaf80d9d9ed7f
root <Me-myself-and-i3>
2024-06-24 13:21:05 +0000

    t

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isalpha - Checks if a character is an alphabetic character.
+* @c: The character to check.
+*
+* Description: This function checks if the given character `c`
+* is an alphabetic character (either lowercase or uppercase).
+*
+* Return: 1 if the character is a letter, lowercase or uppercase, 0 otherwise.
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
