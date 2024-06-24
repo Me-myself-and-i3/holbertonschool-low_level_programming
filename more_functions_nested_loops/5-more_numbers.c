56ed9bca85835d19a198f9ebedf36fa979fedf8b
root <Me-myself-and-i3>
2024-06-24 13:50:50 +0000

    t

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints the alphabet in loxercase followed by a new line.
+* main - check the code
+* Description: print alphabet  x 10 with for
+* print_alphabet_x10 - Prints 10x alphabet in lowercase followed by a new line.
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (int j = 0; J <= 14; J++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n');
+	}
+}
