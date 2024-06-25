0de8201960b62e8e2a89d3f91737a23d88086445
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-25 08:38:38 +0000

    t

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,12 @@
-#include <stdio.h>
+#include <stdio.h
+
+/**
+ * putchar - writes the character c to stdout
+ * The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 /**
  * print_triangle - prints a triangle of '#' characters of given size
@@ -6,18 +14,24 @@
  */
 void print_triangle(int size)
 {
-	int i, j, k;
+    int i, j, k;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 0; j < size - i; j++)
-		{
-			putchar(' ');
-		}
-		for (k = 0; k < i; k++)
-		{
-			putchar('#');
-		}
-		putchar('\n');
-	}
+    if (size <= 0)
+    {
+        putchar('\n');
+        return;
+    }
+
+    for (i = 1; i <= size; i++)
+    {
+        for (j = 0; j < size - i; j++)
+        {
+            putchar(' ');
+        }
+        for (k = 0; k < i; k++)
+        {
+            putchar('#');
+        }
+        putchar('\n');
+    }
 }
