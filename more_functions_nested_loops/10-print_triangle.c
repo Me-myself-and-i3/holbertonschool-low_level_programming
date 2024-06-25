4e867e7dca4a5adc89d2dd01da7e272dfd627ff3
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-25 08:50:20 +0000

    t

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -26,7 +26,7 @@ void print_triangle(int size)
     {
         for (j = 0; j < size - i; j++)
         {
-            putchar(' ');
+            _putchar(' ');
         }
         for (k = 0; k < i; k++)
         {
