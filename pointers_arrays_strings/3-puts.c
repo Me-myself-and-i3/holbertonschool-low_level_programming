9c8a5c49a03db057a37e081b733f46ba25649e3a
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-28 09:38:10 +0000

    r

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Description: This function takes a pointer to a string and returns
+ * the length of the string, excluding the null terminator.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
  * _puts - Prints a string to stdout, followed by a new line.
  * @str: The string to print.
  *
