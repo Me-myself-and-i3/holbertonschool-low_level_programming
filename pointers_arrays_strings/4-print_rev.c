28962a30bd85e9f55e8a6d1b0ef3aaceb205d248
Me-myself-and-i3 <dalimodex@gmail.com>
2024-06-28 09:42:17 +0000

    r

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Description: This function takes a pointer to a string and returns
+ * the length of the string, excluding the null terminator.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
 /**
  * print_rev - Prints a string in reverse, followed by a new line.
  * @s: The string to be printed in reverse.
--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * _strlen - Returns the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Description: This function takes a pointer to a string and returns
+ * the length of the string, excluding the null terminator.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
  * print_array - Prints n elements of an array of integers.
  * @a: The array of integers.
  * @n: The number of elements to be printed.
